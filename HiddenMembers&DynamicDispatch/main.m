f151dcce49ba16cabc82e88c4d89f475f62cb2e9
1985wasagoodyear <1985wasagoodyear>
2019-08-16 09:34:36 -0400

    Initial Commit

---
+++ HiddenMembers&DynamicDispatch/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HiddenMembers&DynamicDispatch
+//
+//  Created by K Y on 8/16/19.
+//  Copyright © 2019 K Y. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
